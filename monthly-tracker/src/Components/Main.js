9ee982695a25b6598e380dc51c74e6114c8b262b
asvindra <aswin1399904@gmail.com>
2021-12-01 12:10:44 +0530

    updated

--- monthly-tracker/src/Components/Main.js
+++ monthly-tracker/src/Components/Main.js
@@ -1,80 +1,71 @@
-import React  from "react";
+import React from "react";
 import Month from './Month';
 
-class Main extends React.Component{
-    constructor(props){
+class Main extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            active:false,
-            activity:[],
-            
+        this.state = {
+            active: false,
+            activity: [],
+
         }
     }
 
-//    handleChange=({target})=>{
-//        this.setState((prevState)=>{
-//         return {
-          
-            
-//            }
-//        })
 
 
-//    }
-
-     handleClear=(event)=>{
-         event.target.reset();
-
-     }
-
-    handleSubmit=(e)=>{
-        e.preventDefault();
-        console.log(e.target.elements.activity);
-        if(e.target.elements.activity.value){
-        this.setState((prevState)=>{
-            return {
-                active:true,
-                activity:[...this.state.activity,e.target.elements.activity.value],
-                
-            }
-           
-        },()=>{
-
-           
-            console.log('state is updated');
-        })
-        setTimeout(()=>{
-            this.handleClear(e);
-        },1);
-       
-       
-       
-
-    }else{
-        e.target.reset();
+    handleClear = (event) => {
+        event.target.reset();
 
     }
 
-}
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.elements.activity);
+        if (e.target.elements.activity.value) {
+            this.setState((prevState) => {
+                return {
+                    active: true,
+                    activity: [...this.state.activity, e.target.elements.activity.value],
+
+                }
+
+            }, () => {
+
+
+                console.log('state is updated');
+            })
+            setTimeout(() => {
+                this.handleClear(e);
+            }, 1);
 
 
 
-    render(){
+
+        } else {
+            e.target.reset();
+
+        }
+
+    }
+
+
+
+    render() {
         console.log(this.state.activity);
-       
+
         return (
             <>
-              <div className="form-control">
-                  <form  onSubmit={this.handleSubmit}>
-                      <input type="text" placeholder="e.g. coding" name="activity"/>
-                      <input type="submit" value="Add Activity"/>
-                  </form>
-              </div>
-              {
-                  this.state.active?this.state.activity.map((elm,i)=>{
-                      return <Month activity={elm} key={i}/>
-                  }):<></>
-              }
+                <div className="form-control">
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="e.g. coding" name="activity" />
+                        <input type="submit" value="Add Activity" />
+                    </form>
+                </div>
+                {
+                    this.state.active ? this.state.activity.map((elm, i) => {
+                        return <Month activity={elm} key={i} />
+                    }) : <></>
+                }
             </>
         )
     }
